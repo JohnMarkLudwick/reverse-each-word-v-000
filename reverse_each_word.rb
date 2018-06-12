f6c24ed6498ad66a3ba4f4fc51192405497a296c
JohnMark <johnmarkludwick@gmail.com>
2018-06-12 17:47:52 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ sentence1 = ("olleH ereht dna woh era ?uoy")
 
 def reverse_each_word(sentence1)
   words = " "
-  example_array.each.reverse do |sentence|
+  sentence1.each.reverse do |sentence|
     puts "#{sentence}"
   end
 
